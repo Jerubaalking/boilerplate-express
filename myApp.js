b186a677255d3eb315e0fba64daaa867c287c4ee
Jerubaalking <gsainyeye@gmail.com>
2023-08-09 23:18:47 +0300

    ip

--- myApp.js
+++ myApp.js
@@ -3,6 +3,14 @@ let app = express();
 require('dotenv').config();
 app.use('/public', express.static(__dirname + '/public'));
 console.log('Hello world');
+const midd = function (req, res, next) {
+    console.log("I'm a middleware...");
+    const method = req.method;
+    const path = req.url;
+    const ip = req.ip;
+    console.log(method, path + ' - ' + ip)
+    next();
+}
 app.get('/', midd, (req, res) => {
     const path = __dirname + '/views/index.html';
     res.sendFile(path);
@@ -14,14 +22,7 @@ app.get('/json', midd, (req, res) => {
     return res.json({ "message": "Hello json" });
 })
 
-const midd = function (req, res, next) {
-    console.log("I'm a middleware...");
-    const method = req.method;
-    const path = req.url;
-    const ip = req.ip;
-    console.log(method, path + ' - ' + ip)
-    next();
-}
+
 
 
 
