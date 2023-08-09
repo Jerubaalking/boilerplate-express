a5ce7d35d0cae00ce6214847d432fe4a191ee029
Jerubaalking <gsainyeye@gmail.com>
2023-08-09 22:57:49 +0300

    p

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let app = express();
+app.use('/public', express.static(__dirname + '/public'));
 console.log('Hello world');
 app.get('/', (req, res) => {
     const path = __dirname + '/views/index.html';
